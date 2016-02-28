81d7c3174679661e7ba297d0f7573dd55ed26da8
Cüneyt Yılmaz <cuneyilmaz@gmail.com>
2016-02-28 16:32:08 +0200

    Projenin şu ana kadar gelinen son hali eklendi.

---
+++ ArtificialReason/js/js.js
@@ -0,0 +1,27 @@
+﻿$('#dropdownMenu1').on('show.bs.dropdown', function () {
+    $('#dropdownMenu1').dropdown('toggle');
+    $('body').animate()
+})
+
+$(document).ready(function () {
+    $('.bxslider').bxSlider();
+});
+
+$('.latest-work .bxslider').bxSlider({
+    nextSelector: '#bx-ileri',
+    prevSelector: '#bx-geri',
+    nextText: '>',
+    prevText: '<',
+    minSlides: 4,
+    maxSlides: 4,
+    slideWidth: 275,
+    pager: false
+});
+
+$('.our-clients .bxslider').bxSlider({
+    nextSelector: '#bx-ileri2',
+    prevSelector: '#bx-geri2',
+    nextText: '>',
+    prevText: '<',
+    pager:false
+});
